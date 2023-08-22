35116df2365e66dfc818d17236c61c80a99a0428
gemrey <gemreyranola@gmail.com>
2023-08-22 09:51:09 +0800

    Hhe

--- src/App.jsx
+++ src/App.jsx
@@ -60,33 +60,33 @@ const Navbar = () => {
       </div>
     </div>
 
-    <div className="collapse collapse-plus bg-base-200 mb-[5em]">
-      <input type="radio" name="my-accordion-3" checked="checked" /> 
-      <div className="collapse-title text-xl font-medium">
-        Click to open this one and close others
-      </div>
-      <div className="collapse-content"> 
-        <p>hello</p>
-      </div>
-    </div>
     <div className="collapse collapse-plus bg-base-200">
-      <input type="radio" name="my-accordion-3" /> 
-      <div className="collapse-title text-xl font-medium">
-        Click to open this one and close others
-      </div>
-      <div className="collapse-content"> 
-        <p>hello</p>
-      </div>
-    </div>
-    <div className="collapse collapse-plus bg-base-200">
-      <input type="radio" name="my-accordion-3" /> 
-      <div className="collapse-title text-xl font-medium">
-        Click to open this one and close others
-      </div>
-      <div className="collapse-content"> 
-        <p>hello</p>
-      </div>
-    </div>
+  <input type="radio" name="my-accordion-3" checked="checked" /> 
+  <div className="collapse-title text-xl font-medium">
+    Click to open this one and close others
+  </div>
+  <div className="collapse-content"> 
+    <p>hello</p>
+  </div>
+</div>
+<div className="collapse collapse-plus bg-base-200">
+  <input type="radio" name="my-accordion-3" /> 
+  <div className="collapse-title text-xl font-medium">
+    Click to open this one and close others
+  </div>
+  <div className="collapse-content"> 
+    <p>hello</p>
+  </div>
+</div>
+<div className="collapse collapse-plus bg-base-200">
+  <input type="radio" name="my-accordion-3" /> 
+  <div className="collapse-title text-xl font-medium">
+    Click to open this one and close others
+  </div>
+  <div className="collapse-content"> 
+    <p>hello</p>
+  </div>
+</div>
 
     <span className="loading loading-spinner loading-xs"></span>
     <span className="loading loading-spinner loading-sm"></span>
